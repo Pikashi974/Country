e861fe5876ea7e045b6c4cf7b5cd1021e9a6067a
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2023-10-31 09:27:19 +0400

    Ajout du filtre et de l'affichage des pays

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,20 @@
 const rangeObject = document.querySelector("#rangeObject");
 const rangeValue = document.querySelector("#rangeValue");
 
-let tableCountries = [];
+let tableMapped;
+let tableCountries;
 
 async function init() {
   tableCountries = await fetchCountries();
+  tableMapped = displayCountries();
+  showCountries(tableMapped.length);
 }
 
 init();
 
 rangeObject.addEventListener("change", function () {
   rangeValue.textContent = rangeObject.value;
+  showCountries(rangeObject.value);
 });
 
 function fetchCountries() {
@@ -19,12 +23,44 @@ function fetchCountries() {
   );
 }
 function displayCountries() {
-  return tableCountries.map((country) => {
-    var objetCountry = {};
-    objetCountry.Drapeau = country.flags.svg;
-    objetCountry.Nom = country.translations.fra.common;
-    objetCountry.Capitale = country.capital;
-    objetCountry.Population = country.population;
-    return objetCountry;
-  });
+  var valeur = document.querySelector("#floatingInput").value;
+
+  return tableCountries
+    .filter(
+      (country) =>
+        (country.capital && country.capital[0].includes(valeur)) ||
+        country.translations.fra.common.includes(valeur)
+    )
+    .map((country) => {
+      var objetCountry = {};
+      objetCountry.Drapeau = country.flags.svg;
+      objetCountry.Nom = country.translations.fra.common;
+      objetCountry.Capitale = country.capital;
+      objetCountry.Population = country.population;
+      return objetCountry;
+    });
+}
+
+function showCountries(max) {
+  document.querySelector("#showCountries").innerHTML = "";
+  for (let index = 0; index < max; index++) {
+    const element = tableMapped[index];
+    // On vérifie que la capitale a bien été renseignée
+    let capitale = element.Capitale ? element.Capitale[0] : "Inconnu";
+    document.querySelector("#showCountries").innerHTML +=
+      `<div class="list-group m-1">
+        <img src="` +
+      element.Drapeau +
+      `" width=100>
+            <h5>` +
+      element.Nom +
+      `</h5>
+             <small>` +
+      capitale +
+      `</small>
+      <small>Population: ` +
+      element.Population +
+      `</small>
+      </div>`;
+  }
 }

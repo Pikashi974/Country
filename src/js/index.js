37457f48ccf23ee76a0d05af01865128c7c57372
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2023-10-31 08:28:09 +0400

    Ajout de fetchCountries

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 const rangeObject = document.querySelector("#rangeObject");
 const rangeValue = document.querySelector("#rangeValue");
 
+let tableCountries = [];
+
 function init() {}
 
 init();
@@ -8,3 +10,9 @@ init();
 rangeObject.addEventListener("change", function () {
   rangeValue.textContent = rangeObject.value;
 });
+
+function fetchCountries() {
+  return fetch("https://restcountries.com/v3.1/all").then((response) =>
+    response.json()
+  );
+}
